722f96f659f7cc2abf2e972ca1a91806af7c98e0
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-08-03 00:41:00 +0200

    updating city module

--- models/city.py
+++ models/city.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+"""This module defines the City class for the HBNB project
+
+The City class represents a city and contains attributes related to a city
+such as its name, state ID, and the relationship with the Place class
+"""
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    """City class for the HBNB project
+
+    Attributes:
+        __tablename__ (str): The table name for the City class
+        name (str): The name of the city
+        state_id (str): The state ID to which the city belongs
+        places (relationship): The relationship with the Place class, representing
+            all places associated with this city
+    """
+
+    __tablename__ = "cities"
+
     name = Column(String(128), nullable=False)
+    state_id = Column(
+        String(60), ForeignKey("states.id", ondelete="CASCADE"), nullable=False
+    )
+    places = relationship(
+        "Place", backref="cities", cascade="all, delete", passive_deletes=True
+    )
