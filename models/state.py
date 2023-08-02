bbc1d2e0a361e6f0fc79ada63427cb6e4bb28f10
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-08-02 21:23:47 +0200

    updating state module

--- models/state.py
+++ models/state.py
@@ -1,10 +1,11 @@
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+#!/usr/bin/python3
 
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """State class"""
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete-orphan')
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
