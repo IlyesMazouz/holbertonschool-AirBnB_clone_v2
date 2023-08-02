7ad3e93fcb052b500c488b7d462fc17bc3a634e8
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-08-02 21:29:52 +0200

    adding db storage file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-
+"""
+DBStorage Class Implementation
+"""
 from os import getenv
 import models
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
+
 
 class DBStorage:
     """New storage using SQLAlchemy"""
@@ -14,14 +17,17 @@ class DBStorage:
 
     def __init__(self):
         """Initialization of the DBStorage"""
-        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
-        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
-        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
-        HBNB_ENV = getenv('HBNB_ENV')
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB),
-            pool_pre_ping=True)
+        HBNB_MYSQL_USER = getenv("HBNB_MYSQL_USER")
+        HBNB_MYSQL_PWD = getenv("HBNB_MYSQL_PWD")
+        HBNB_MYSQL_HOST = getenv("HBNB_MYSQL_HOST")
+        HBNB_MYSQL_DB = getenv("HBNB_MYSQL_DB")
+        HBNB_ENV = getenv("HBNB_ENV")
+        self.__engine = create_engine(
+            "mysql+mysqldb://{}:{}@{}/{}".format(
+                HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB
+            ),
+            pool_pre_ping=True,
+        )
         if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
@@ -31,13 +37,13 @@ class DBStorage:
         if cls:
             objs = self.__session.query(cls).all()
             for obj in objs:
-                key = obj.__class__.__name__ + '.' + obj.id
+                key = obj.__class__.__name__ + "." + obj.id
                 all_dict[key] = obj
         else:
             for itr in models.classes:
                 objs = self.__session.query(models.classes[itr]).all()
                 for obj in objs:
-                    key = obj.__class__.__name__ + '.' + obj.id
+                    key = obj.__class__.__name__ + "." + obj.id
                     all_dict[key] = obj
         return all_dict
 
