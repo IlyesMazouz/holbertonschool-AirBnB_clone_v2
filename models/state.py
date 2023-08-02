046e24d83a4cb35cb46f6f13355011bcf9fe5a36
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-08-02 21:41:27 +0200

    state module update

--- models/state.py
+++ models/state.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""
-state module
-"""
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """State class"""
-
-    __tablename__ = "states"
+    """ State class """
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    cities = relationship("City", backref="state", cascade="all, delete")
