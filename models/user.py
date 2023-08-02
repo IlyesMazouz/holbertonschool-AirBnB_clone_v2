e077d97ceef4476dd67d0c11ce1d9b3917f0f2b2
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-08-03 01:33:15 +0200

    updating user module

--- models/user.py
+++ models/user.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""This module defines the User class, which inherits from BaseModel"""
+"""This module defines the User class"""
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
-    """The User class, contains user information"""
+    """The User class, which inherits from BaseModel and Base"""
 
     __tablename__ = 'users'
 
@@ -14,3 +16,6 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship("Place", backref="user", cascade="all, delete",
+                          passive_deletes=True)
