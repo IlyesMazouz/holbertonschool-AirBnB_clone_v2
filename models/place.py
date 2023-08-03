7b0b2e185bb6365fdf05d00e9d18c7bc149d953d
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-08-03 14:52:19 +0200

    updating place module

--- models/place.py
+++ models/place.py
@@ -2,25 +2,26 @@
 """This module defines the Place class"""
 
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class Place(BaseModel, Base):
     """The Place class, which inherits from BaseModel and Base"""
 
-    __tablename__ = 'places'
+    __tablename__ = "places"
 
-    city_id = Column(String(60), ForeignKey('cities.id', ondelete="CASCADE"),
-                     nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id', ondelete="CASCADE"),
-                     nullable=False)
+    city_id = Column(
+        String(60), ForeignKey("cities.id", ondelete="CASCADE"), nullable=False
+    )
+    user_id = Column(
+        String(60), ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
