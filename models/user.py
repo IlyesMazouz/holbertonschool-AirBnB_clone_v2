d2ba196de24dfe9d7a60c3a5d9858dd81aacd0a3
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-08-03 14:52:35 +0200

    updating user module

--- models/user.py
+++ models/user.py
@@ -4,18 +4,18 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class User(BaseModel, Base):
     """The User class, which inherits from BaseModel and Base"""
 
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
 
-    places = relationship("Place", backref="user", cascade="all, delete",
-                          passive_deletes=True)
+    places = relationship(
+        "Place", backref="user", cascade="all, delete", passive_deletes=True
+    )
