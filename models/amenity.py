35d6fd67ae979696353f23cd478f8a563ab000c9
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-08-04 10:06:28 +0200

    updating amenity module

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This module defines the Amenity class"""
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """The Amenity class, which inherits from BaseModel and Base"""
+
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
