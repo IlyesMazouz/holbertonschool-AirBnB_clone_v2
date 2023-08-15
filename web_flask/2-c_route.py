1c18ac6ed3aade402a1e01cd6f7b479eb3fa651c
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-08-15 20:01:28 +0200

    adding an instance

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,21 +7,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
+
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
-@app.route('/hbnb', strict_slashes=False)
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'"""
     return "HBNB"
 
-@app.route('/c/<text>', strict_slashes=False)
+
+@app.route("/c/<text>", strict_slashes=False)
 def c_is_fun(text):
     """Displays 'C <text>'"""
-    text = text.replace('_', ' ')
+    text = text.replace("_", " ")
     return "C {}".format(text)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
